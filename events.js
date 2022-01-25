d553dca3c04bcf1a1deaaa2f0429465c4312cce6
Vib-UX <vibchess123@gmail.com>
2022-01-25 14:05:35 +0530

    Events.js

---
+++ events.js
@@ -0,0 +1,24 @@
+const EventEmitter = require("events");
+
+const celebrity = new EventEmitter()
+
+// Observer 1 ---> Fan
+celebrity.on("race", (result)=>{
+    (result === "win") ? console.log("Congratulation, well done") : console.log("No worries we will get it Next Time!")
+})
+
+
+// Observer 2 ---> Nemesis
+celebrity.on("race", (result)=>{
+    result === "win" ? console.log("I could have done it better!") : console.log("I was right to not bet on you.") 
+})
+
+
+// Process Global object which also generated from events
+process.on("exit", (code)=>{
+    console.log(`Execution ${code} has been finished `)
+})
+
+// Emit the celebrity event
+celebrity.emit("race", "win")
+
