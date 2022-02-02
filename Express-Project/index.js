04a918aee215cfc0b917b8963183bb43592329b2
Vib-UX <vibchess123@gmail.com>
2022-02-02 20:27:22 +0530

    Routing

--- Express-Project/index.js
+++ Express-Project/index.js
@@ -2,6 +2,22 @@ const express = require('express')
 
 const app = express();
 const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    res.send({
+        id:1,
+        name: 'Web Browser'
+    })
+})
+
+app.get('/messages', (req,res)=>{
+    res.send("<ul><li>Helloo Web</li></ul>");
+})
+
+app.post('/messages', (req,res)=>{
+    console.log('Updating messages...')
+})
+
 app.listen(PORT,()=>{
     console.log(`Listening on PORT.... ${PORT}`)
 })
