2257c22582b1016a9db27d660af270054f87660e
Vib-UX <vibchess123@gmail.com>
2022-02-03 21:15:18 +0530

    nodemon-dev

--- Express-Project/index.js
+++ Express-Project/index.js
@@ -3,11 +3,43 @@ const express = require('express')
 const app = express();
 const PORT = 3000;
 
+const friends = [
+    {
+        id:0,
+        name: 'Web Browser'
+    },
+    {
+        id:1,
+        name: 'User'   
+    },
+    {
+        id:2,
+        name: 'Postman'
+    }
+];
+
 app.get('/',(req,res)=>{
     res.send({
-        id:1,
+        id:0,
         name: 'Web Browser'
-    })
+    });
+})
+
+app.get('/friends',(req,res)=>{
+    res.json(friends)
+})
+
+// Parameterized route
+app.get('/friends/:friendsID', (req,res)=>{
+    const friendId = +req.params.friendsID;     // here '+' implicitly converts it into a Number
+    const friendShare = friends[friendId];
+    if(friendShare){
+        res.json(friendShare);
+    } else {
+        res.status('404').json({
+            error: "Friend does not exist"
+        })
+    }
 })
 
 app.get('/messages', (req,res)=>{
