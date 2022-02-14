2dd81b5fe5f223b9d67a08f181123d5ecd1c89c4
Vib-UX <vibchess123@gmail.com>
2022-02-14 19:16:36 +0530

    Dataset load await/async

--- NASA-Project/server/src/models/planets.model.js
+++ NASA-Project/server/src/models/planets.model.js
@@ -1,5 +1,50 @@
-const planets =[];
+const { parse } = require('csv-parse')
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-    planets
+const habitablePlanet = []
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] >0.36 && planet['koi_insol']<1.11
+    && planet['koi_prad']<1.6;
+}
+
+function loadPlanetsData(){
+    // Its a good practice to stream large datasets
+    return new Promise((resolve,reject) =>{
+        fs.createReadStream(path.join(__dirname,'..','..','data', 'kepler_data.csv'))
+        /*
+            Pipe function is meant to connect a readable stream source to readable 
+            stream destination.
+
+            In our case process the data in readable format using parse 
+        */
+        .pipe(parse({
+            comment: '#',
+            columns: true,
+        })) 
+        .on('data', (data)=>{
+            if(isHabitablePlanet(data)){
+                habitablePlanet.push(data)
+            }
+        })
+        .on('error', (err)=>{
+            console.log(err)
+            reject(err)
+        })
+        .on('end', ()=>{
+            
+            console.log(habitablePlanet.length);
+            console.log('The file is ended!')
+            resolve();
+        })
+    })
+}
+
+// loadPlanetsData();
+
+module.exports={
+    loadPlanetsData,
+    planets: habitablePlanet,
 }
--- NASA-Project/server/src/server.js
+++ NASA-Project/server/src/server.js
@@ -4,7 +4,15 @@ const app = require('./app');
 const PORT = process.env.PORT || 8000;
 
 const  server =  http.createServer(app);
+const {loadPlanetsData} = require('./models/planets.model');
 
-server.listen(PORT, ()=>{
-    console.log(`Server listening to PORT: ${PORT}...`)
-})
+
+async function loadServer(){
+    await loadPlanetsData();
+    server.listen(PORT, ()=>{
+        console.log(`Server listening to PORT: ${PORT}...`)
+    })
+}
+
+loadServer();
+
