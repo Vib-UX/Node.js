0fc3bbf397d553cc8861f9278f47f8e26e871e71
Vib-UX <vibchess123@gmail.com>
2022-02-15 06:13:52 +0530

    static build server side

--- NASA-Project/server/src/app.js
+++ NASA-Project/server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const planetsRouter = require('./routes/palnets/planets.router')
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +12,13 @@ app.use(cors({
 // Middleware
 app.use(express.json());
 
+// Static server listen
+app.use(express.static(path.join(__dirname,'..','public_1')));
+
 app.use(planetsRouter);
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public_1','index.html'));
+})
 
 module.exports= app;
--- NASA-Project/server/src/server.js
+++ NASA-Project/server/src/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
+
 const PORT = process.env.PORT || 8000;
 
 const  server =  http.createServer(app);
