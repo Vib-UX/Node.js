6c8367937bdac5af86a3a956d7b5521555b7d1b5
Vib-UX <vibchess123@gmail.com>
2022-03-18 17:13:46 +0530

    MongoDB Connect

--- NASA-Project/server/src/server.js
+++ NASA-Project/server/src/server.js
@@ -1,14 +1,24 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose')
 
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = `mongodb+srv://nasa-api:CBea5kR5ateyNBs7@cluster0.oipat.mongodb.net/nasa?retryWrites=true&w=majority`
+
 
 const  server =  http.createServer(app);
 const {loadPlanetsData} = require('./models/planets.model');
 
+mongoose.connection.once('open',()=>{
+    console.log('MongoDB connection ready!')
+})
+mongoose.connection.on('error',(err)=>{
+    console.error(err)
+})
 
 async function loadServer(){
+    await mongoose.connect(MONGO_URL)
     await loadPlanetsData();
     server.listen(PORT, ()=>{
         console.log(`Server listening to PORT: ${PORT}...`)
