28e890f413f2c558087f038e6bcec4f3cabea9c2
Vib-UX <vibchess123@gmail.com>
2022-03-19 17:55:30 +0530

    Launch Schema

---
+++ NASA-Project/server/src/models/launches.mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const launchSchema = new mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required: true,
+        default: 100,
+        min: 100,
+        max: 999,
+    },
+    launchDate: {
+        type: Date,
+        required: true
+    },
+    mission: {
+        type: String,
+        required: true
+    },
+    target: {
+        type: String,
+        required: true
+    },
+    customers: {
+        type: [String]
+    },
+    upcoming: {
+        type: Boolean,
+        required: true,
+    },
+    success: {
+        type: Boolean,
+        required: true,
+        default: true,
+    }
+})
