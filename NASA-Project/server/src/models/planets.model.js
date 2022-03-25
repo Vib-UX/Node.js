c23cc05e531a649d4003250b7e1847ad0ace9d8e
Vib-UX <vibchess123@gmail.com>
2022-03-25 15:50:34 +0530

    mongo-db planetModel refactor

--- NASA-Project/server/src/models/planets.model.js
+++ NASA-Project/server/src/models/planets.model.js
@@ -4,8 +4,6 @@ const path = require('path');
 
 const planets = require('./planets.mongo')
 
-const habitablePlanet = []
-
 function isHabitablePlanet(planet){
     return planet['koi_disposition'] === 'CONFIRMED' 
     && planet['koi_insol'] >0.36 && planet['koi_insol']<1.11
@@ -28,19 +26,16 @@ function loadPlanetsData(){
         })) 
         .on('data', async(data)=>{
             if(isHabitablePlanet(data)){
-                await planets.create({
-                    //insert + update = upsert
-                    keplerName: data.kepler_name
-                })
+                await savePlanet(data);
             }
         })
         .on('error', (err)=>{
             console.log(err)
             reject(err)
         })
-        .on('end', ()=>{
-            
-            console.log(habitablePlanet.length);
+        .on('end', async ()=>{
+            const countPlanetsFound = await getAllPlanets()
+            console.log(countPlanetsFound.length);
             console.log('The file is ended!')
             resolve();
         })
@@ -49,7 +44,23 @@ function loadPlanetsData(){
 
 // loadPlanetsData();
 async function getAllPlanets(){
-    return await planets.find({});
+    return await planets.find({},{
+        "_id":0, '__v':0
+    });
+}
+
+async function savePlanet(planet){
+    try{
+        await planets.updateOne({
+            keplerName: planet.kepler_name
+        },{
+            keplerName: planet.kepler_name
+        },{
+            upsert: true,
+        })
+    } catch (err){
+        console.error(`Could not save planet ${err}`)
+    }
 }
 
 module.exports={
