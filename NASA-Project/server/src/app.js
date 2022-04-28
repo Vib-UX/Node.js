6afc2b3ebe1c6ca2848ce3f9f405ed5140cd930b
Vib-UX <vibchess123@gmail.com>
2022-04-28 18:11:01 +0530

    Versioning of API --frntend build

--- NASA-Project/server/src/app.js
+++ NASA-Project/server/src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const planetsRouter = require('./routes/palnets/planets.router')
-const launchesRouter = require('./routes/launches/launches.router');
 const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
 const app = express();
+
+const api = require('./routes/api');
 
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -19,8 +19,9 @@ app.use(express.json());
 // Static server listen
 app.use(express.static(path.join(__dirname,'..','public_1')));
 
-app.use('/planets',planetsRouter);
-app.use('/launches',launchesRouter);
+// App using Version-1 api
+app.use('/v1',api);
+
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'..','public_1','index.html'));
 })
